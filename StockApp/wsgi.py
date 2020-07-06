b35e85dd90599436358522ae9b998f6ba0b975ac
Ricardo Martins <cacolorde@gmail.com>
2020-07-06 10:10:55 -0300

    Heroku 3rd Attempt

--- StockApp/wsgi.py
+++ StockApp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockPWA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StockApp.settings')
 
 application = get_wsgi_application()
